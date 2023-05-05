0bdf2d6a7e06aaafe53a79d240f0f1d8e801e477
kumankoishi.py <7iggif@naver.com>
2023-05-05 13:43:18 +0900

    draft(counter): try double counter

--- src/Main.ts
+++ src/Main.ts
@@ -7,9 +7,8 @@ import {
 export default function Main() {
   const rootElement = document.createElement('div');
   function render() {
-    rootElement.innerHTML = '';
     rootElement.appendChild(Text({
-      text: `Current Value: ${counter()}`,
+      text: `Current Value 1: ${counter()}`,
     }));
     rootElement.appendChild(Button({
       text: 'Increase',
@@ -19,18 +18,36 @@ export default function Main() {
       text: 'Decrease',
       onClick: decreaseCounter,
     }));
+
+    rootElement.appendChild(Text({
+      text: `Current Value 2: ${counter2()}`,
+    }));
+    rootElement.appendChild(Button({
+      text: 'Increase',
+      onClick: increaseCounter2,
+    }));
+    rootElement.appendChild(Button({
+      text: 'Decrease',
+      onClick: decreaseCounter2,
+    }));
   }
 
   const { useState } = useFunctionComponentHooks({ rootElement, render });
 
   const [counter, setCounter] = useState<number>(0);
-
   function increaseCounter(): void {
     setCounter(counter() + 1);
   }
-
   function decreaseCounter(): void {
     setCounter(counter() - 1);
+  }
+
+  const [counter2, setCounter2] = useState<number>(0);
+  function increaseCounter2(): void {
+    setCounter2(counter2() + 1);
+  }
+  function decreaseCounter2(): void {
+    setCounter2(counter2() - 1);
   }
 
   render();
