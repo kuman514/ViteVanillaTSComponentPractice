a3606b024f2d80ba8303db821dc143ac4f66aaf5
kumankoishi.py <7iggif@naver.com>
2023-05-12 22:16:22 +0900

    feat(end-tip-arrow): add end tip arrow icon

--- src/Main.ts
+++ src/Main.ts
@@ -1,74 +1,12 @@
-import {
-  useFunctionComponentHooks,
-} from '^/hooks/useFunctionComponentHooks';
-import Checkbox, { CheckStatus } from './components/atoms/Checkbox';
-
-interface CheckboxChain {
-  main: CheckStatus;
-  sub: CheckStatus[];
-}
+import EndTipArrow from '^/components/atoms/EndTipArrow';
 
 export default function Main() {
   const rootElement = document.createElement('div');
   function render() {
-    const { main, sub } = checkStatus();
-
-    rootElement.appendChild(Checkbox({
-      checkStatus: main,
-      onClick: handleOnClickMainCheckbox,
+    rootElement.appendChild(EndTipArrow({
+      width: '100px',
+      height: '100px',
     }));
-
-    for (let i = 0; i < sub.length; i++) {
-      rootElement.appendChild(Checkbox({
-        checkStatus: sub[i],
-        onClick: () => handleOnClickSubCheckbox(i),
-      }));
-    }
-  }
-
-  const { useState } = useFunctionComponentHooks({ rootElement, render });
-
-  const [checkStatus, setCheckStatus] = useState<CheckboxChain>({
-    main: CheckStatus.NONE,
-    sub: [CheckStatus.NONE, CheckStatus.NONE, CheckStatus.NONE],
-  });
-
-  function handleOnClickMainCheckbox() {
-    const { main, sub } = checkStatus();
-
-    if (main === CheckStatus.FULL) {
-      setCheckStatus({
-        main: CheckStatus.NONE,
-        sub: sub.map(() => CheckStatus.NONE),
-      });
-    } else {
-      setCheckStatus({
-        main: CheckStatus.FULL,
-        sub: sub.map(() => CheckStatus.FULL),
-      });
-    }
-  }
-
-
-  function handleOnClickSubCheckbox(i: number) {
-    const { sub } = checkStatus();
-    const newSub = Array.from(sub);
-
-    if (newSub[i] === CheckStatus.FULL) {
-      newSub[i] = CheckStatus.NONE;
-    } else {
-      newSub[i] = CheckStatus.FULL;
-    }
-
-    const isFull: boolean = newSub.every(cur => cur === CheckStatus.FULL);
-    const isNone: boolean = newSub.every(cur => cur === CheckStatus.NONE);
-
-    const newMain = isFull ? CheckStatus.FULL : (isNone ? CheckStatus.NONE : CheckStatus.PARTIAL);
-
-    setCheckStatus({
-      main: newMain,
-      sub: newSub,
-    });
   }
 
   render();
